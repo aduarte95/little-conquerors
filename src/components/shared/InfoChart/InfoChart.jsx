4401c45eef683e2ff8f9c2d28b3ca6f9e52c5bb1
aduarte95 <aduarte.roldan@gmail.com>
2020-02-10 21:14:23 -0600

    Info chart animation

--- src/components/shared/InfoChart/InfoChart.jsx
+++ src/components/shared/InfoChart/InfoChart.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './InfoChart.scss';
 import { CSSTransition } from 'react-transition-group';
 
 
 function InfoChart({children, className}) {
+  const [showInfoChart, setShowInfoChart] = useState(true);
   
   return (
-    <CSSTransition in={true}
+    <div> { 
+    <CSSTransition in={showInfoChart}
     timeout={10000}
     classNames="info-chart"
     unmountOnExit
-    appear>
+    appear
+    exit
+    onEnter={()=> setTimeout(() => {
+      setShowInfoChart(false);
+    }, 9000)}>
       <div className={"info-chart-container glowing-box " + className}>
           <p className="info-chart-container__text">
               {children}
           </p>
       </div>
     </CSSTransition>
+  }</div>
   );
 }
 
